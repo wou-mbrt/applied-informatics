de8860dd0169434230e835217567d37085792535
Павел <PaulEfimov2001@yandex.ru>
2021-02-08 14:06:27 +0300

    first commit

---
+++ Лаба1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Лаба1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
