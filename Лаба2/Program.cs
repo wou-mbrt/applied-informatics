2cc01813152334f79df898c9f9117d0cbb53bc21
Павел <PaulEfimov2001@yandex.ru>
2021-02-25 03:36:03 +0300

    lab_2 done

--- Лаба2/Program.cs
+++ Лаба2/Program.cs
@@ -13,13 +13,50 @@ namespace Лаба2
              * x2,y2 это в мат. формулах i-2
              * x3,y3 это в мат. формулах i-3
             */
-            double x = 3.5;
-            double y = Y_formula(x);
-            double h = 0.5;
+            double h = 0.1;
+            double x3 = 0.1;
+            double y3 = Y_formula(x3);
+            double x2 = x3 + h;
+            double y2 = RungeKutta(h, x3, y3);
+            double x1 = x2 + h;
+            double y1 = RungeKutta(h, x2, y2);
+            double x = x1 + h;
+            double y = RungeKutta(h, x1, y1);
+            double xi = x + h;
             double yi = RungeKutta(h, x, y);
 
-            Console.WriteLine("h = {0} \t x = {1}", h, x);
-            Console.WriteLine("y = {0:F3} \t yi = {1:F3}", y, yi);
+            Console.WriteLine("Начальные значения");
+            Console.WriteLine("h = {0:F2} \t x3 = {1:F2} \t y3 = {2:F2}", h, x3, y3);
+            Console.WriteLine("h = {0:F2} \t x2 = {1:F2} \t y2 = {2:F2}", h, x2, y2);
+            Console.WriteLine("h = {0:F2} \t x1 = {1:F2} \t y1 = {2:F2}", h, x1, y1);
+            Console.WriteLine("h = {0:F2} \t x  = {1:F2} \t y  = {2:F2}", h, x, y);
+
+            Console.WriteLine("Метод Рунге-Кутта:");
+            Console.WriteLine("yi = {0:F3}", yi);
+            Console.WriteLine();
+
+            Console.WriteLine("Метод прогноза:");
+            yi = Prediction(h, x, y, x1, y1, x2, y2, x3, y3);
+            Console.WriteLine("yi = {0:F3}", yi);
+            Console.WriteLine();
+
+            Console.WriteLine("Метод коррекции:");
+            yi = Correction(h, xi, yi, x, y, x1, y1, x2, y2);
+            Console.WriteLine("yi = {0:F3}", yi);
+            Console.WriteLine();
+
+            Console.WriteLine("Метод Адамса:");
+            yi = Adams(h, x, y, x1, y1, x2, y2, x3, y3);
+            Console.WriteLine("yi = {0:F3}", yi);
+            Console.WriteLine();
+            
+            Console.WriteLine("Метод последовательных приближений:");
+            yi = ConsecutiveApproximations(x3,y3,3);
+            Console.WriteLine("yi = {0:F3}", yi);
+
+
+
+            Console.WriteLine("");
         }
         /// <summary>
         /// Формула для нахождения Y, можно менять
@@ -39,7 +76,7 @@ namespace Лаба2
         /// <returns>double результат расчета</returns>
         public static double FFormula(double x, double y)
         {
-            return Math.Pow(x, y);
+            return x + y;
         }
         /// <summary>
         /// Формула для одного K
@@ -50,9 +87,7 @@ namespace Лаба2
         /// <returns>double K</returns>
         public static double KFormula(double h, double x, double y)
         {
-            double k;
-            k = h * FFormula(x, y);
-            return k;
+            return h * FFormula(x, y);
         }
         /// <summary>
         /// формула Рунге-Кутта, рассчитывает Yi
@@ -68,11 +103,10 @@ namespace Лаба2
             double k2 = KFormula(h, x + h / 2, y + k1 / 2);
             double k3 = KFormula(h, x + h, y + k2);
 
-            double yi = y + (k0 + 2 * k1 + 2 * k2 + k3) / 6;
-            return yi;
+            return y + (k0 + 2 * k1 + 2 * k2 + k3) / 6;
         }
         /// <summary>
-        /// Метод Прогноза и коррекции, расчитывает Yi
+        /// Прогноз, расчитывает Yi
         /// </summary>
         /// <param name="h">h</param>
         /// <param name="x">x</param>
@@ -84,10 +118,26 @@ namespace Лаба2
         /// <param name="x3">x3</param>
         /// <param name="y3">y3</param>
         /// <returns>double Yi</returns>
-        public static double PredictionAndCorrection(double h, double x, double y, double x1, double y1, double x2, double y2, double x3, double y3)
+        public static double Prediction(double h, double x, double y, double x1, double y1, double x2, double y2, double x3, double y3)
         {
-            double yi = y * h / 24 * (55 * FFormula(x, y) - 59 * FFormula(x1, y1) + 37 * FFormula(x2, y2) - 9 * FFormula(x3, y3));
-            return yi;
+            return y * h / 24 * (55 * FFormula(x, y) - 59 * FFormula(x1, y1) + 37 * FFormula(x2, y2) - 9 * FFormula(x3, y3));
+        }
+        /// <summary>
+        /// Прогноз, расчитывает Yi
+        /// </summary>
+        /// <param name="h">h</param>
+        /// <param name="x_i">x i+1</param>
+        /// <param name="y_i">y i+1</param>
+        /// <param name="x">x</param>
+        /// <param name="y">y</param>
+        /// <param name="x1">x i-1</param>
+        /// <param name="y1">y i-1</param>
+        /// <param name="x2">x i-2</param>
+        /// <param name="y2">y i-2</param>
+        /// <returns>double yi</returns>
+        public static double Correction(double h, double x_i, double y_i, double x, double y, double x1, double y1, double x2, double y2)
+        {
+            return y * h / 24 * (9 * FFormula(x_i, y_i) - 19 * FFormula(x, y) + 5 * FFormula(x1, y1) + FFormula(x2, y2));
         }
         /// <summary>
         /// Метод Адамса, рассчитывает Yi
@@ -108,8 +158,22 @@ namespace Лаба2
             double deltaF2 = FFormula(x, y) - 2 * FFormula(x1, y1) + FFormula(x2, y2);
             double deltaF3 = FFormula(x, y) - 3 * FFormula(x1, y1) + 3 * FFormula(x2, y2) - FFormula(x3, y3);
 
-            double yi = y + h * y + h * h / 2 * deltaF1 + 5 * h * h * h / 12 * deltaF2 + 3 * h * h * h * h / 8 * deltaF3;
-            return yi;
+            return y + h * y + h * h / 2 * deltaF1 + 5 * h * h * h / 12 * deltaF2 + 3 * h * h * h * h / 8 * deltaF3;
+        }
+        /// <summary>
+        /// Расчет интеграла в 4 задаче
+        /// </summary>
+        /// <param name="x0">X</param>
+        /// <param name="steps">Количество шагов</param>
+        /// <returns>Сумма интеграла</returns>
+        public static double Integral(double x0, double steps)
+        {
+            double integral = 0;
+            for (int i = 1; i < steps+1; i++)
+            {
+                integral += Math.Pow(x0, i) / i;
+            }
+            return integral;
         }
         /// <summary>
         /// Метод последовательных приближений, рассчитывается Yi
@@ -119,10 +183,9 @@ namespace Лаба2
         /// <param name="x">Конечный X</param>
         /// <param name="y">Конечный y</param>
         /// <returns>double yi</returns>
-        public static double ConsecutiveApproximations(double x0, double y0, double x, double y)
+        public static double ConsecutiveApproximations(double x0, double y0, double n)
         {
-            double yi = y0 + FFormula(x, y) - FFormula(x0, y);
-            return yi;
+            return y0 + Integral(x0, n);
         }
     }
 }
